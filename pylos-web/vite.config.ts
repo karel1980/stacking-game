aafe159352a9470af9dd32a4f4dcb959082a78ec
Karel Vervaeke <karel@vervaeke.info>
2026-04-08 21:51:47 +0200

    Set up github actions

--- pylos-web/vite.config.ts
+++ pylos-web/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  base: './',
+  base: process.env.GITHUB_ACTIONS ? '/stacking-game/' : './',
   optimizeDeps: {
     exclude: ['onnxruntime-web'],
   },
